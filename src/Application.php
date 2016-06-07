29f73c7866904e74149161913aef1fa2929c46e8
schwaen <s.muhle@plajo.de>
2016-06-07 15:57:26 +0200

    allow json in request(default)

--- src/Application.php
+++ src/Application.php
@@ -34,6 +34,9 @@ class Application extends BaseApplication
         if (!isset($this['app.version'])) {
             $this['app.version'] = self::VERSION;
         }
+        if (!isset($this['request.accept_json'])) {
+            $this['request.accept_json'] = true;
+        }
 
          $this->extend('route_factory', function($route_factory) use ($app) {
              if ($route_factory instanceof Route) {
@@ -46,6 +49,15 @@ class Application extends BaseApplication
         $this->cli(new HttpCommand());
         foreach ($services as $service => $services_values) {
             $this->register(new $service(), $services_values);
+        }
+        
+        if ($this['request.accept_json'] === true) {
+            $this->before(function(Request $request) {
+                if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+                    $data = json_decode($request->getContent(), true);
+                    $request->request->replace(is_array($data) ? $data : array());
+                }
+            });
         }
     }
 
